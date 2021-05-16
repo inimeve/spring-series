0e48836a012287e16c72882107b1e147db9e961c
inimeve <inigo.merino.vegas@gmail.com>
2021-05-17 00:01:32 +0200

    add mongo query

---
+++ mongo-spring-boot/src/main/java/com/example/accessingdatamongodb/CustomerController.java
@@ -0,0 +1,24 @@
+package com.example.accessingdatamongodb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    List<Customer> findAllCustomers() {
+        return this.customerRepository.findAll();
+    }
+}
