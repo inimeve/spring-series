2ca6ea74fd9bc3a1f554fcecaecaaa8a5d0265ee
inimeve <inigo.merino.vegas@gmail.com>
2021-05-18 00:07:33 +0200

    initial jaxb project

---
+++ demo-jaxb/src/main/java/com/example/demojaxb/DemoJaxbApplication.java
@@ -0,0 +1,21 @@
+package com.example.demojaxb;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJaxbApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJaxbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mongo");
+		
+	}
+
+}
---
+++ demo-jaxb/src/test/java/com/example/demojaxb/DemoJaxbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojaxb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJaxbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
