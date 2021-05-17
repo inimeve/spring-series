c3df16499549ca20bb51fdcaf32ebfbdfeb3c06f
inimeve <inigo.merino.vegas@gmail.com>
2021-05-18 00:50:26 +0200

    add marshaller and unmarshaller sample

--- demo-jaxb/src/main/java/com/example/demojaxb/DemoJaxbApplication.java
+++ demo-jaxb/src/main/java/com/example/demojaxb/DemoJaxbApplication.java
@@ -1,11 +1,19 @@
 package com.example.demojaxb;
 
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoJaxbApplication implements CommandLineRunner{
+public class DemoJaxbApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJaxbApplication.class, args);
@@ -13,9 +21,34 @@ public class DemoJaxbApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Mongo");
-		
+		try {
+			// Create marshaller
+			JAXBContext jaxbContext = JAXBContext.newInstance(Fruit.class);
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+			Fruit o = new Fruit();
+			o.setId(1);
+			o.setName("Banana");
+			o.setPrice("9.99");
+
+            StringWriter sw = new StringWriter();
+            jaxbMarshaller.marshal(o, sw);
+            String xmlContent = sw.toString();
+            System.out.println( xmlContent );
+			
+			// Create unmarshaller
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            
+            Fruit fruit = (Fruit) jaxbUnmarshaller.unmarshal(new StringReader(xmlContent));
+            System.out.println(fruit);
+			
+			
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
---
+++ demo-jaxb/src/main/java/com/example/demojaxb/Fruit.java
@@ -0,0 +1,52 @@
+package com.example.demojaxb;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+// order of the fields in XML
+// @XmlType(propOrder = {"price", "name"})
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Fruit {
+
+    @XmlAttribute
+    int id;
+
+    @XmlElement(name = "n")
+    String name;
+
+    String price;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Fruit [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+    
+}
