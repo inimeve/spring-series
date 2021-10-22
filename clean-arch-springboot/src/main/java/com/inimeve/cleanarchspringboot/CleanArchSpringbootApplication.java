d2e8a6894b45a71829afb436c4763c3e64196e66
inimeve <inigo.merino.vegas@gmail.com>
2021-10-23 01:13:24 +0200

    add clean-arch-springboot project

---
+++ clean-arch-springboot/src/main/java/com/inimeve/cleanarchspringboot/CleanArchSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.inimeve.cleanarchspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchSpringbootApplication.class, args);
+	}
+
+}
---
+++ clean-arch-springboot/src/test/java/com/inimeve/cleanarchspringboot/CleanArchSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inimeve.cleanarchspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
